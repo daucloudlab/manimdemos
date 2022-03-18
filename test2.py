792af09477dcec92a8a77395f603c5fe4e4c2cc8
Daulet <daucloudlab@gmail.com>
2022-03-18 14:41:58 +0600

    Updater Demo 2

--- test2.py
+++ test2.py
@@ -53,3 +53,24 @@ class UpdaterDemo(Scene):
         mathtext.clear_updaters()
         self.play(rectangle.animate.shift(LEFT*2+UP*1), run_time=6)
         self.wait()
+
+
+class UpdaterDemo2(Scene):
+    def construct(self):
+        rectangle = RoundedRectangle(stroke_width = 8, stroke_color = WHITE,
+        fill_color = BLUE_B, width = 4.5, height = 2).shift(UP*3+LEFT*4)
+
+        mathtext = always_redraw(lambda: MathTex("\\frac{3}{4} = 0.75"
+        ).set_color_by_gradient(GREEN, PINK).set_height(1.5).move_to(rectangle.get_center()))
+       
+        self.play(FadeIn(rectangle))
+        self.play(Write(mathtext))
+        self.play(rectangle.animate.shift(RIGHT*1.5+DOWN*5), run_time = 6)
+        self.wait()
+        
+        # self.play(Create(VGroup(rectangle, mathtext)))
+        # self.play(rectangle.animate.shift(RIGHT*1.5+DOWN*5), run_time = 6)
+        # self.wait()
+        mathtext.clear_updaters()
+        self.play(rectangle.animate.shift(LEFT*2+UP*1), run_time=6)
+        self.wait()
