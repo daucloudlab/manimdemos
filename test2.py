fd9eff6ddd319a20daaf8bf969ad0abb6b320e6f
Daulet <daucloudlab@gmail.com>
2022-03-18 13:29:46 +0600

    Fitting Demo created

--- test2.py
+++ test2.py
@@ -15,4 +15,23 @@ class AnimationDemo(Scene):
         self.play(box.animate.shift(UP*3), run_time = 2)
         self.play(box.animate.shift(DOWN*5+LEFT*5), run_time = 2)
         self.play(box.animate.shift(UP*1.5+RIGHT*1), run_time = 2)
-        
+        self.wait()
+
+class FittingDemo(Scene):
+    def construct(self):
+        axes = Axes(x_range = [-3,3,1], y_range = [-3,3,1], x_length = 6, y_length = 6)
+        axes.to_edge(LEFT, buff=0.5)
+
+        circle = Circle(stroke_color = YELLOW, stroke_width=6, 
+        fill_color = RED_C, fill_opacity = 0.8)
+        circle.set_width(2).to_edge(DR, buff=0)
+
+        triangle = Triangle(stroke_color = ORANGE, stroke_width = 10,
+        fill_color=GREY).set_height(2).shift(DOWN*3+RIGHT*3)
+
+        self.play(Write(axes))
+        self.play(DrawBorderThenFill(circle))
+        self.play(circle.animate.set_width(1))
+        self.play(Transform(circle, triangle), run_time = 3)
+
+        self.wait()
