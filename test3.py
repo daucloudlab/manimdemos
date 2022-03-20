c90349ebe37478a3a99abf18e998aa9bd6f1b19d
Daulet <daucloudlab@gmail.com>
2022-03-20 10:55:41 +0600

    test3.py added. Braces Demo

---
+++ test3.py
@@ -0,0 +1,58 @@
+from manim import *
+
+class OpeningManim(Scene):
+    def construct(self):
+        title = Tex(r"This is some \LaTeX")
+        basel = MathTex(r"\sum_{n=1}^\infty \frac{1}{n^2} = \frac{\pi^2}{6}")
+        VGroup(title, basel).arrange(DOWN)
+        self.play(
+            Write(title),
+            FadeIn(basel, shift=DOWN),
+        )
+        self.wait()
+
+        transform_title = Tex("That was a transform")
+        transform_title.to_corner(UP + LEFT)
+        self.play(
+            Transform(title, transform_title),
+            LaggedStart(*(FadeOut(obj, shift=DOWN) for obj in basel)),
+        )
+        self.wait()
+
+        grid = NumberPlane()
+        grid_title = Tex("This is a grid", font_size=72)
+        grid_title.move_to(transform_title)
+
+        self.add(grid, grid_title)  # Make sure title is on top of grid
+        self.play(
+            FadeOut(title),
+            FadeIn(grid_title, shift=UP),
+            Create(grid, run_time=3, lag_ratio=0.1),
+        )
+        self.wait()
+        
+class UsingBraces(Scene):
+    #Using braces to group text together
+    def construct(self):
+        eq1A = Text("4x + 3y")
+        eq1B = Text("=")
+        eq1C = Text("0")
+        eq2A = Text("5x -2y")
+        eq2B = Text("=")
+        eq2C = Text("3")
+        eq1B.next_to(eq1A,RIGHT)
+        eq1C.next_to(eq1B,RIGHT)
+        eq2A.shift(DOWN)
+        eq2B.shift(DOWN)
+        eq2C.shift(DOWN)
+        eq2A.align_to(eq1A,LEFT)
+        eq2B.align_to(eq1B,LEFT)
+        eq2C.align_to(eq1C,LEFT)
+
+        eq_group=VGroup(eq1A,eq2A)
+        braces=Brace(eq_group,LEFT)
+        eq_text = braces.get_text("A pair of equations")
+
+        self.add(eq1A, eq1B, eq1C)
+        self.add(eq2A, eq2B, eq2C)
+        self.play(GrowFromCenter(braces),Write(eq_text))
