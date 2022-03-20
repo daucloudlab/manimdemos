37ed58178d032cfd79fe2d96e00d1fd7942ef0ec
Daulet <daucloudlab@gmail.com>
2022-03-20 16:31:38 +0600

    BasicAnimationDemo is added in test3.py

--- test3.py
+++ test3.py
@@ -116,3 +116,23 @@ class GroupingDemo(Scene):
         stars = VGroup(*[Star(color=YELLOW, fill_opacity=0.5).scale(0.2) for _ in range(20)])
         stars.arrange_in_grid(4, 5, buff=0.2).to_edge(LEFT)
         self.add(stars)
+
+import numpy as np
+from colour import Color
+class BasicAnimationDemo(Scene):
+    def construct(self):
+        polys = VGroup(
+            *[RegularPolygon(5, radius = 1, fill_opacity=0.5,
+            color = Color(hue=j/5, saturation=1, luminance=0.5)) for j in range(5)]
+        ).arrange(RIGHT)
+        
+        self.play(DrawBorderThenFill(polys), run_time = 2)
+        self.play(
+            Rotate(polys[0], PI, rate_func = linear),
+            Rotate(polys[1], PI, rate_func = smooth),
+            Rotate(polys[2], PI, rate_func = lambda t : np.sin(t*PI)),
+            Rotate(polys[3], PI, rate_func = there_and_back),
+            Rotate(polys[4], PI, rate_func = lambda t : 1 - abs(1-2*t)),
+            run_time = 2
+        )
+        self.wait()
