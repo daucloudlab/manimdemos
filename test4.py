adcbbc2969d921dc727db85a50f9135d8c4c2322
Daulet <daucloudlab@gmail.com>
2022-05-16 17:17:31 +0600

    Moving Around demo

--- test4.py
+++ test4.py
@@ -135,3 +135,12 @@ class PointMovingOnShapes3(Scene):
         # done! wait and exit
         self.wait(2)
         
+class MovingAround(Scene):
+    def construct(self):
+        square = Square(color = BLUE, fill_opacity=1)
+        self.play(square.animate.shift(LEFT))
+        self.play(square.animate.set_fill(ORANGE))
+        self.play(square.animate.scale(0.3))
+        self.play(square.animate.rotate(0.4))
+
+
