2e19f361491bb65d038c7d3ad571582089361b2b
Daulet <daucloudlab@gmail.com>
2022-05-18 15:10:27 +0600

    Moving Group of dots to destination

--- test4.py
+++ test4.py
@@ -202,3 +202,16 @@ class MovingDots(Scene):
         self.play(y.animate.set_value(4))
         self.wait()
 
+
+class MovingGroupToDestination(Scene):
+    def construct(self):
+        group = VGroup(Dot(LEFT), Dot(ORIGIN), Dot(RIGHT, color=RED), Dot(2*RIGHT)).scale(1.4)
+        dest = Dot([4, 3, 0], color = YELLOW)
+
+        self.add(group, dest)
+        self.wait(3)
+        self.play(group.animate.shift(dest.get_center() - group[2].get_center()))
+        self.wait()
+
+
+
