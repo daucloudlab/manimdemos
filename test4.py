66cc2d4946acb5c4453603eac01ee7eba7f6a834
Daulet <daucloudlab@gmail.com>
2022-05-19 19:01:14 +0600

    MovingCameraOnGraph

--- test4.py
+++ test4.py
@@ -410,3 +410,19 @@ class MovingAndZoomingCamera(MovingCameraScene):
         self.play(self.camera.frame.animate.move_to(t).set(width=t.width*2))
 
         self.play(self.camera.frame.animate.move_to(ORIGIN).set(width=14))
+
+class MovingCameraOnGraph(MovingCameraScene):
+    def construct(self):
+        self.camera.frame.save_state()
+
+        ax = Axes(x_range=[-1, 10], y_range=[-1, 10])
+        graph = ax.plot(lambda x: np.sin(x), color=WHITE, x_range=[0, 3 * PI])
+
+        dot_1 = Dot(ax.i2gp(graph.t_min, graph))
+        dot_2 = Dot(ax.i2gp(graph.t_max, graph))
+        self.add(ax, graph, dot_1, dot_2)
+
+        self.play(self.camera.frame.animate.scale(0.5).move_to(dot_1))
+        self.play(self.camera.frame.animate.move_to(dot_2))
+        self.play(Restore(self.camera.frame))
+        self.wait()
